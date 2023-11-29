327839fd0f875853874571e07b0db27f00efdc92
vboxuser <vboxuser@sssss.myguest.virtualbox.org>
2023-11-29 21:02:11 +0300

    added a new project

--- ksi.rs
+++ ksi.rs
@@ -1,3 +1,13 @@
-fn main(){
-println!("hello,world");
+ use std::io;
+
+fn main(){ 
+
+    println!("guess the number");
+    
+    let mut guess = String::new();
+
+    io::stdin()
+ .read_line(&mut guess);
+ 
+  println!("u guessed right,{guess}");
 }
